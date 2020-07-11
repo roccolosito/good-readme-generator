99e0681b0c4bf6df1498841e6af9dc546a9f3932
Rocco Losito <roccolosito@gmail.com>
2020-07-11 11:50:39 -0400

    added Requires and started building questions const

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const questions = [
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
 
-];
+const questions = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "username",
+                message: "Please enter your GitHub username."
+            },
+        ],
+        )
+}
 
 function writeToFile(fileName, data) {
-}
+},
 
 function init() {
 
-}
+},
 
-init();
+init()
