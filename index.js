de4b8e60c4b014670b34bcb2a478044bee123da3
Rocco Losito <roccolosito@gmail.com>
2020-07-11 16:43:30 -0400

    added all of the questions

--- index.js
+++ index.js
@@ -1,25 +1,71 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMd = require("./utils/generateMarkdown.js");
 
-const questions = () => {
-    inquirer
-        .prompt([
+const questions = [
             {
                 type: "input",
                 name: "username",
                 message: "Please enter your GitHub username."
             },
-        ],
-        )
-}
-
+            {
+                type: "input",
+                name: "title",
+                message: "What is the name of the project?",
+                default: "No project title given."
+            },
+            {
+                type: "input",
+                name: "description",
+                message: "Please describe the project.",
+                default: "No project description given."
+            },
+            {
+                type: "input",
+                name: "installation",
+                message: "What are the installation instructions?",
+                default: "No installation instructions given."
+            },
+            {
+                type: "input",
+                name: "usage",
+                message: "How is this project used?",
+                default: "No usage examples given."
+            },
+            {
+                type: "list",
+                name: "license",
+                message: "Which license do you want to use for this project?",
+                choices: ["Apache", "MIT"],
+                default: "Apache"
+            },
+            {
+                type: "input",
+                name: "contributing",
+                message: "What guidelines would like to give to contributors?",
+                default: "No notes for contributions given."
+            },
+            {
+                type: "input",
+                name: "tests",
+                message: "How do you run tests?",
+                default: "No testing instructions given."
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please provide your GitHub email address",
+                default: "Blank."
+            },
+            
+        ];
+        
 function writeToFile(fileName, data) {
-},
+}
 
 function init() {
 
-},
+}
 
-init()
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,13 @@
 function generateMarkdown(data) {
+  let title = data.title;
+  let description = data.description;
+  let contents = data.contents;
+  let installation = data.installation;
+  let usage = data.usage;
+  let license = data.license;
+  let contributing = data.contributing;
+  let tests = data.tests;
+
   return `
 # ${data.title}
 
